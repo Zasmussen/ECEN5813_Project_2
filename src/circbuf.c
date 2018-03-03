7ad60b7e91d8860dc056a7440bef530ec8105320
zasmussen <zaas3933@colorado.edu>
2018-03-02 21:54:45 -0700

    Finished circbuf and changed some flags in circbuf

--- src/circbuf.c
+++ src/circbuf.c
@@ -12,43 +12,128 @@
 #include <stdint.h>
 #include <stdlib.h>
 #include "circbuf.h"
-
-CB_e CB_init(uint8_t * buffPtr, uint32_t length)
+#include <stdio.h>
+CB_e CB_init(CB_t ** buffPtr, uint32_t length)
 {
-  CB_e error = SUCCESS;
-  return error;
+  if(length <= 0)
+  {
+    return NO_LENGTH;
+  }
+  // Dynamically allocate CB struct
+
+  (*buffPtr) = (CB_t *)malloc(sizeof(CB_t));
+
+  if(buffPtr == NULL)
+  {
+    return CB_NULL;
+  }
+  // Dynamically allocate CB array space
+  (*buffPtr)->basePtr = (uint8_t *) malloc(length);
+  if((*buffPtr)->basePtr == NULL)
+  {
+    return CB_NULL;
+  }
+  // Initialize values in structure
+  (*buffPtr)->head = (*buffPtr)->basePtr;
+  (*buffPtr)->tail = (*buffPtr)->basePtr;
+  (*buffPtr)->length = length;
+  (*buffPtr)->count = 0;
+  return SUCCESS;
 }
 
 
-CB_e CB_destroy(uint8_t * buffPtr)
+CB_e CB_destroy(CB_t * buffPtr)
 {
-  CB_e error = SUCCESS;
-  return error;
+  if(buffPtr == NULL || buffPtr->basePtr == NULL)
+  {
+    return CB_NULL;
+  }
+  free(buffPtr->basePtr);
+  buffPtr->basePtr = NULL;
+  free(buffPtr);
+  buffPtr = NULL;
+  return SUCCESS;
 }
 
 
-CB_e CB_buffer_add_item(uint8_t * buffPtr, uint8_t data)
+CB_e CB_buffer_add_item(CB_t * buffPtr, uint8_t data)
 {
-  CB_e error = SUCCESS;
-  return error;
+  if(buffPtr->basePtr == NULL || buffPtr->head == NULL || buffPtr->tail == NULL || buffPtr == NULL)
+  {
+    return CB_NULL;
+  }
+  if(buffPtr->count == buffPtr->length)
+  {
+    return CB_FULL;
+  }
+
+  // Check if first item added, should only happen once
+  if(buffPtr->head == buffPtr->tail && !*(buffPtr)->head)
+  {
+    *(buffPtr)->head = data;
+    buffPtr->count++;
+  }
+  else  // Not first item
+  {
+    if(buffPtr->head == (buffPtr->basePtr + buffPtr->length - 1)) // Check if circular
+    {
+      buffPtr->head = buffPtr->basePtr;
+    }
+    else
+    {
+
+      buffPtr->head++;
+    }
+
+    *(buffPtr)->head = data;
+    buffPtr->count++;
+  }
+  return SUCCESS;
 }
 
 
-CB_e CB_buffer_remove_item(uint8_t * buffPtr, uint8_t * value)
+CB_e CB_buffer_remove_item(CB_t * buffPtr, uint8_t * value)
 {
-  CB_e error = SUCCESS;
-  return error;
+  if(buffPtr->basePtr == NULL || buffPtr->head == NULL || buffPtr->tail == NULL || buffPtr == NULL)
+  {
+    return CB_NULL;
+  }
+
+  if(!(buffPtr->count))
+  {
+    return CB_EMPTY;
+  }
+
+  *(value) = *(buffPtr)->tail;
+
+  // Check for circular
+  if(buffPtr->tail == (buffPtr->basePtr + buffPtr->length -1))
+  {
+    buffPtr->tail = buffPtr->basePtr;
+  }
+  else
+  {
+    buffPtr->tail++;
+  }
+  return SUCCESS;
 }
 
 
-__attribute__((always_inline))inline CB_e CB_is_full(uint8_t * buffPtr)
+__attribute__((always_inline))inline CB_e CB_is_full(CB_t * buffPtr)
 {
-  CB_e error = SUCCESS;
-  return error;
+  if(buffPtr->basePtr == NULL || buffPtr->head == NULL || buffPtr->tail == NULL || buffPtr == NULL)
+  {
+    return CB_NULL;
+  }
+  return (buffPtr->count == buffPtr->length);
 }
 
-__attribute__((always_inline))inline CB_e CB_is_empty(uint8_t * buffPtr)
+
+__attribute__((always_inline))inline CB_e CB_is_empty(CB_t * buffPtr)
 {
-  CB_e error = SUCCESS;
-  return error;
+  if(buffPtr->basePtr == NULL || buffPtr->head == NULL || buffPtr->tail == NULL || buffPtr == NULL)
+  {
+    return CB_NULL;
+  }
+  return (buffPtr->count == 0);
 }
--- src/main.c
+++ src/main.c
@@ -11,20 +11,50 @@
  */
 #include "project1.h"
 #include "GPIO.h"
+#include "circbuf.h"
+#include <stdio.h>
 int main(void)
 {
-  uint32_t i;
+  uint32_t i = 0;
   #ifdef PROJECT1
     project1();
   #endif
 
   #ifdef PROJECT2
-    GPIO_Configure();
+    //GPIO_Configure();
   #endif
-  GPIO_Configure();
+  //GPIO_Configure();
+  CB_t * buffer = NULL;
+  CB_init(&buffer,10);
+  CB_buffer_add_item(buffer,1);
+  CB_buffer_add_item(buffer,2);
+  CB_buffer_add_item(buffer,3);
+  CB_buffer_add_item(buffer,4);
+  CB_buffer_add_item(buffer,5);
+  CB_buffer_add_item(buffer,6);
+  CB_buffer_add_item(buffer,7);
+  CB_buffer_add_item(buffer,8);
+  CB_buffer_add_item(buffer,9);
+  CB_buffer_add_item(buffer,10);
+  uint8_t x;
+  CB_buffer_remove_item(buffer,&x);
+  CB_buffer_remove_item(buffer,&x);
+  CB_buffer_remove_item(buffer,&x);
+  CB_buffer_remove_item(buffer,&x);
+  CB_buffer_remove_item(buffer,&x);
+  printf("Num: %d\n",x);
+//  printf("%d\n",*(buffer->tail+1));
+  while(*(buffer->tail+i))
+  {
+    printf("%d\n",*(buffer->tail+i));
+    i++;
+  }
+
+
+
   while(1)
   {
-    Toggle_Red_LED();
+    //Toggle_Red_LED();
     for(i=0;i<1000000;i++);
   }
 }
